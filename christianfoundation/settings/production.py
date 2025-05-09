489d9c8f7c76e6e795c0cc1bbd15d327de479c18
Katalizatori <elguga175@gmail.com>
2025-05-09 22:11:07 +0200

    trying to fix images in static folder

--- christianfoundation/settings/production.py
+++ christianfoundation/settings/production.py
@@ -26,6 +26,10 @@ DATABASES = {
 # Static files (WhiteNoise)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Points to your static folder
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Collection target
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (if you have user uploads)
